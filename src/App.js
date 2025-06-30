49004b40e6d09b4b34b43677873007b5e9baffd2
Joshua504 <chukwuebukajosh8@gmail.com>
2025-06-30 22:19:37 +0100

    I

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Building Materials App</h1>
+        <p>Welcome to your React application!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
